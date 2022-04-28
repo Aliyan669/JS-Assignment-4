b6fe2dff42973535c92af0a99f5b69ccb0909e6b
Aliyan669 <tboy02722@gmail.com>
2022-04-27 19:09:22 -0700

    JS Assignment 4

---
+++ app.js
@@ -0,0 +1,74 @@
+// // chapter 25 - 30  ////
+// //
+// //Ques No : 1 (A)
+// //
+// var num = 3.45214
+// document.write("Number : "+num+"</br>")
+// // Ques No : 1 (B)
+// var round = Math.round(num);
+// document.write("Round off value : "+round+"</br>")
+// // Ques No : 1 (c)
+// var floor = Math.floor(num);
+// document.write("floor value : "+floor+"</br>")
+// // Ques No : 1 (D)
+// var ceil = Math.ceil(num);
+// document.write("Ceil value : "+ceil+"</br>")
+// //
+// //Ques no : 02 (A)
+// //
+// var num = -2.763 
+// document.write("Number : "+num+"</br>")
+// // Ques No : 2 (B)
+// var round = Math.round(num);
+// document.write("Round off value : "+round+"</br>")
+// // Ques No : 2 (c)
+// var floor = Math.floor(num);
+// document.write("floor value : "+floor+"</br>")
+// // Ques No : 2 (D)
+// var ceil = Math.ceil(num);
+// document.write("Ceil value : "+ceil+"</br>")
+// //
+// //Ques No : 03
+// //
+//  var  num = -4
+//  document.write("The Absolute value of "+ num + " is "+ Math.abs(num))
+//  
+// Ques No : 04
+//
+//  var random= Math.floor( Math.random() * 7);
+//  var random1 = Math.floor( Math.random() * 7);
+// document.write("Random Dice Value is "+random+"</br>");
+// document.write("Random Dice Value is "+random1);
+//
+// Ques No : 05
+//
+// var random= Math.floor( Math.random() * 3);
+// if (random == 2){
+//     document.write(random+"</br> Random coin value :  Head")
+// }else{
+//     document.write(random+"</br> Random coin value :  Tails")   
+// }
+//
+// Ques No : 06
+//
+// var random= Math.floor( Math.random() * 100);
+//     document.write("Random Number Between 1 to 100  : "+random)
+//
+// Ques No : 07
+//
+//
+// Ques No : 08
+//
+// var secret= Math.floor( Math.random() * 11)
+//  var random =+prompt("Enter a Number")
+//         if(random == secret){
+//          alert("congratulate")
+//      }else{
+//         alert("Try Again")
+// }
+    
+ 
+
+
+
+
